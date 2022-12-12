57b668589a6454964bcae2ddb8157f9bf2ed42dd
Christabel <christabel.masakhwe@student.moringaschool.com>
2022-12-12 12:57:43 +0300

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  resources :admissions, only: [:index, :create, :destroy]
+  resources :hospitals, only: [:index, :show, :update]
+ 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  post "/signup", to: "patients#create"
+  get "/me", to: "patients#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # Defines the root path route ("/")
   # root "articles#index"
 end
